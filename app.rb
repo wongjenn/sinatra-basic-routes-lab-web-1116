7eab82a824e952b2e4fb7529f72bb873be9a4770
wongjenn <wongjenn@yahoo.com>
2016-12-15 11:40:28 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Jennifer."
+  end
+
+  get '/hometown' do
+    "My hometown is NYC."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'Rolling in the Deep'."
+  end
+
 end
